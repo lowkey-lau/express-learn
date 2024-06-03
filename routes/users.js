7b350ff73770179ecddffd828fa2d5fd4e166e43
lowkey_lau <a317605747@gmail.com>
2024-06-03 13:39:47 +0800

    first commit

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+// router.get('/', function (req, res, next) {
+//   const params = req.query
+//   res.send(params)
+// });
+
+
+
+router.get('/', function(req, res){
+  res.render('users', {
+    title: "EJS example",
+    header: "Some users"
+  });
+});
+
+
+module.exports = router;
